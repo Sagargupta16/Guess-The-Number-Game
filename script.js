6b5a7206d59784d6463c725af6363bb5029185e5
Sagar Gupta <sg85207@gmail.com>
2022-06-18 10:09:06 +0530

    18-06-2022

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 let guesses;
 // Variable for store the correct random number 
 let correctNumber;
+// Score
+let score;
 
 window.onload = function() {
     initGame();
@@ -14,23 +16,24 @@ window.onload = function() {
 }
 
 function playGame(){
-  let numberGuess = document.getElementById("number-guess").value;
-  saveGuessHistory(numberGuess);
-  displayHistory();
-  displayResult(numberGuess);
+    let numberGuess = document.getElementById("number-guess").value;
+    displayResult(numberGuess);
+    saveGuessHistory(numberGuess);
+    displayHistory();
 }
 
 // Initialize a new game by resetting all values and content on the page
 function initGame(){
-  correctNumber = getRandomNumber();
-  guesses = [];
-  displayHistory();
-  resetResultContent();
+    score = 100;
+    correctNumber = getRandomNumber();
+    guesses = [];
+    displayHistory();
+    resetResultContent();
 }
 
 // Reset the results list display
 function resetResultContent(){
-  document.getElementById("result").innerHTML = "";
+    document.getElementById("result").innerHTML = "";
 }
 
 // Return random number between 1 and 1000
@@ -39,12 +42,12 @@ function getRandomNumber(){
    * Math.random returns a number between 0 and 1,
    * and that's why we multiply it by 1000
    */
-  return Math.floor((Math.random() * 1000) + 1);
+    return Math.floor((Math.random() * 1000) + 1);
 }
 
 // Save the user guess entered from the input
 function saveGuessHistory(guess) {
-  guesses.push(guess);
+    guesses.push(guess);
 }
 
 // Display history in HTML 
@@ -64,13 +67,16 @@ function displayHistory() {
 
 // Display the result in HTML
 function displayResult(numberGuess){
-  if(numberGuess > correctNumber) {
-    showNumberAbove()
-  } else if (numberGuess < correctNumber){
-    showNumberBelow()
-  } else {
-    showYouWon()
-  }
+    if(numberGuess > correctNumber) {
+      showNumberAbove(numberGuess-correctNumber)
+    } 
+    else if (numberGuess < correctNumber){
+      showNumberBelow(correctNumber-numberGuess)
+    } 
+    else {
+      showYouWon()
+    }
+    score--;
 }
 
 // Retrieve the dialog based on if the guess is wrong or correct 
@@ -78,10 +84,10 @@ function getDialog(dialogType, text){
   let dialog;
   switch(dialogType){
     case "warning":
-      dialog = "<div class='alert alert-warning' role='alert'>"
+      dialog = "<div class='alert alert-warning' role='alert'>";
       break;
     case "won":
-      dialog = "<div class='alert alert-success' role='alert'>"
+      dialog = "<div class='alert alert-success' role='alert'>";
       break;
   }
   dialog += text;
@@ -90,19 +96,34 @@ function getDialog(dialogType, text){
 }
 
 function showYouWon(){
-  const text = "Awesome job, you got it!"
-  let dialog = getDialog('won', text)
+    if(score<0){
+        score=0;
+    }
+    const text = "Awesome job, you got it! Your Score is : "+score+"/100" ;
+    let dialog = getDialog('won', text);
+    document.getElementById("result").innerHTML = dialog;
+}
+
+function showNumberAbove(diff){
+    let text; 
+    if(diff>50){
+        text = "Your guess is too high!";
+    }
+    else{
+        text = "Your guess is high!";
+    }
+  let dialog = getDialog('warning', text);
   document.getElementById("result").innerHTML = dialog;
 }
 
-function showNumberAbove(){
-  const text = "Your guess is too high!"
-  let dialog = getDialog('warning', text)
-  document.getElementById("result").innerHTML = dialog;
-}
-
-function showNumberBelow(){
-  const text = "Your guess is too low!"
-  let dialog = getDialog('warning', text)
+function showNumberBelow(diff){
+  let text; 
+    if(diff>50){
+        text = "Your guess is too low!";
+    }
+    else{
+        text = "Your guess is low!";
+    }
+  let dialog = getDialog('warning', text);
   document.getElementById("result").innerHTML = dialog;
 }
